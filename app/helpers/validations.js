0a92b5d15e55f4af943ecc73ce214f78c3225206
sebaraba <rabasebastians@gmail.com>
2020-05-07 22:43:57 +0100

    Add:
    * status codes;
    * initial validations;

---
+++ app/helpers/validations.js
@@ -0,0 +1,53 @@
+import env from '../../env';
+
+/**
+   * isValidEmail helper method
+   * @param {string} email
+   * @returns {Boolean} True or False
+   */
+const isValidEmail = (email) => {
+  const regEx = /\S+@\S+\.\S+/;
+  return regEx.test(email);
+};
+
+/**
+   * validatePassword helper method
+   * @param {string} password
+   * @returns {Boolean} True or False
+   */
+const validatePassword = (password) => {
+    const regEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    return regEx.test(password);
+};
+
+/**
+   * isEmpty helper method
+   * @param {string, integer} input
+   * @returns {Boolean} True or False
+   */
+const isEmpty = (input) => {
+  if (input === undefined || input === '') {
+    return true;
+  }
+  if (input.replace(/\s/g, '').length) {
+    return false;
+  } return true;
+};
+
+/**
+   * empty helper method
+   * @param {string, integer} input
+   * @returns {Boolean} True or False
+   */
+const empty = (input) => {
+  if (input === undefined || input === '') {
+    return true;
+  }
+};
+
+export {
+  isValidEmail,
+  validatePassword,
+  isEmpty,
+  empty
+};
