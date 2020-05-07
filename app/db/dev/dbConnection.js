8fec672905898a8ec3c9cb1e151134ca16fc6ec0
sebaraba <rabasebastians@gmail.com>
2020-05-07 21:34:31 +0100

    Add:
    * db config;
    * users table;
    * pg query;

---
+++ app/db/dev/dbConnection.js
@@ -0,0 +1,76 @@
+import pool from './pool';
+
+pool.on('connect', () => {
+    console.log('connected to the db');
+});
+
+pool.on('remove', () => {
+    console.log('client removed');
+    process.exit(0);
+});
+
+/**
+ * CREATE users Table
+ */
+
+const createUsersTable = () => {
+    const createUsersQuery = `CREATE TABLE IF NOT EXISTS users
+    (id SERIAL PRIMARY KEY, 
+    email VARCHAR(100) UNIQUE NOT NULL, 
+    first_name VARCHAR(100), 
+    last_name VARCHAR(100), 
+    password VARCHAR(100) NOT NULL,
+    created_on DATE NOT NULL)`;
+
+    pool.query(createUsersQuery)
+    .then((res) => {
+        console.log(res);
+        pool.end();
+    })
+    .catch((err) => {
+        console.log(err);
+        pool.end();
+    });
+};
+
+/**
+ * DROP Users Table
+ */
+
+const dropUsersTable = () => {
+    const dropUsersQuery = `DROP TABLE IF EXISTS user`;
+    pool.query(dropUsersQuery)
+    .then((res) => {
+        console.log(res);
+        pool.end;
+    })
+    .catch((err) => {
+        console.log(err);
+        pool.end;
+    });
+};
+
+/**
+ * CREATE All Tables
+ * this is method is intended for a latter use when we need to export several CREATES/DROPS
+ */
+const createAllTables = () => {
+    createUsersTable();
+};
+
+  
+/**
+ * DROP All Tables
+ * this is method is intended for a latter use when we need to export several CREATES/DROPS
+ */
+
+const dropAllTables = () => {
+    dropUsersTable();
+};
+
+export {
+    createAllTables,
+    dropAllTables
+}
+
+require('make-runnable');
---
+++ app/db/dev/dbQuery.js
@@ -0,0 +1,23 @@
+import pool from `./pool`;
+
+export default {
+    /**
+     * DB Query
+     * @param {object} rwq
+     * @param {object} res
+     * @returns {object} object
+     */
+
+
+    query(queryText, params) {
+        return new Promise((resolve, reject) => {
+            pool.query(queryText, params)
+            .then((res) => {
+                resolve(res);
+            })
+            .catch((err) => {
+                reject(err);
+            });
+        });
+    }
+}
