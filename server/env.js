bc4041d5a25bd35ab6688be3ff2a71be92005d04
sebaraba <rabasebastians@gmail.com>
2020-05-10 19:18:37 +0100

    Serve static UI and fix docker

--- server/env.js
+++ server/env.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  database_url: process.env.DATABASE_URL,
+  database_url: 'postgresql://sebaraba:P@ssword1!@localhost:5432/nodeApplicationDB',
   test_database_url: process.env.TEST_DATABASE_URL,
   secret: process.env.SECRET,
   port: process.env.PORT || 5000,
